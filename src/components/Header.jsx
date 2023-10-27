cb8ea71637ff4cd45cd17bfc771d53ddb2b93fa5
unknown <alejandromarulanda702@gmail.com>
2023-10-27 00:49:02 -0500

    saludo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
+    return(
+        <h1>hola</h1>
+    )
 };
 
 export default Header;
